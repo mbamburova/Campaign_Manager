d11c0d6f17d5b424afece3f53e5e1cfa2e329225
Michaela Bamburova <michaela.bamburova@gmail.com>
2016-04-23 13:57:45 +0200

    Create MainWindow

---
+++ src/main/java/campaignmanager/MainWindow.java
@@ -0,0 +1,37 @@
+package campaignmanager;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Michaela Bamburová on 23.04.2016.
+ */
+public class MainWindow extends javax.swing.JFrame {
+
+
+    private JPanel Mission;
+    private JPanel Hero;
+    private JButton deleteButton;
+    private JTextField textField1;
+    private JCheckBox availableCheckBox;
+    private JSpinner spinner1;
+    private JSpinner spinner2;
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> { // zde použito funcionální rozhraní
+                    JFrame frame = new JFrame();
+                    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    frame.setTitle("Campaign Manager");
+                    frame.setVisible(true);
+                }
+        );
+
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+
+
+    }
+}
+
